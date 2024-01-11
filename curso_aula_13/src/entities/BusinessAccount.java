95ba0605c8ff623711d33b2cbcd1fc8d9dbe4ae3
Luís Felipe Barros dos Santos <86605874+Luis-Felipe-B-Santos@users.noreply.github.com>
2024-01-11 01:55:33 -0300

    Adicionada aula sobre @Override

---
+++ curso_aula_13/src/entities/BusinessAccount.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class BusinessAccount extends Account{
+	
+	private Double loanLimit;
+	
+	BusinessAccount(){
+		super();
+	}
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan1(double ammount) { //usando um metodo herdado no metodo da subclasse
+		if (ammount <= loanLimit) {
+			deposit(ammount);
+		}
+	}
+	
+	public void loan2(double ammount) { //usando um atributo protegido da superclasse
+		if (ammount <= loanLimit) {
+			balance += ammount - 10;
+		}
+	}
+}
