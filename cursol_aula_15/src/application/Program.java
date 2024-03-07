8198185b8fa48835f9ec4e2a748506752d360d14
Luís Felipe Barros dos Santos <86605874+Luis-Felipe-B-Santos@users.noreply.github.com>
2024-03-07 05:52:03 -0300

    Adicionada soluçao ruim para aula 15

--- cursol_aula_15/src/application/Program.java
+++ cursol_aula_15/src/application/Program.java
@@ -33,12 +33,13 @@ public class Program {
 			checkIn = fmt.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = fmt.parse(sc.next());
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
-		}
-		
-		
-		
+			
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
+			}else {
+				System.out.println("Reservation: " + reservation);}
+			}
 		sc.close();
 	}
 
